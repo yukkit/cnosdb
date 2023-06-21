d02b3c1247df8786e72dfe1dce78784e97d95e07
yukkit.zhang <yukkit.zhang@cnosdb.com>
2023-06-21 18:27:39 +0800

    try to fix the problem of connection kv failure

--- meta/src/model/meta_admin.rs
+++ meta/src/model/meta_admin.rs
@@ -4,6 +4,7 @@ use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Arc;
+use std::time::Duration;
 
 use config::Config;
 use models::auth::role::TenantRoleIdentifier;
@@ -153,6 +154,16 @@ impl AdminMeta {
                     msg: err.to_string(),
                 }
             })?;
+
+        let connector = connector
+            .connect_timeout(Duration::from_secs(20))
+            .timeout(Duration::from_secs(20))
+            // Disable Nagle's Algorithm since we don't want packets to wait
+            .tcp_nodelay(true)
+            .tcp_keepalive(Option::Some(Duration::from_secs(3600)))
+            .http2_keep_alive_interval(Duration::from_secs(300))
+            .keep_alive_timeout(Duration::from_secs(20))
+            .keep_alive_while_idle(true);
 
         let channel = connector
             .connect()
